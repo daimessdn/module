6b63344446b0a945759673b53863ce8fc534374f
daimessdn@gmail.com <daimessdn@gmail.com>
2023-06-23 15:38:44 +0700

    applied variable module

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import mymodule
 
+# applying function module
 mymodule.greeting("world")
+
+# applying variable module
+a = mymodule.person1["name"]
+print(a)
--- mymodule.py
+++ mymodule.py
@@ -1,2 +1,10 @@
+# function module
 def greeting(name):
     print("hello, %s!" % (name))
+
+# variable module
+person1 = {
+    "name": "bambang",
+    "age": 24,
+    "country": "Indonesia",
+}
